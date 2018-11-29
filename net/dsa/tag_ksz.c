8b251a793db389dcc54e9c2deeeb610609d3047d
Marek Vasut <marex@denx.de>
2018-11-29 18:10:34 +0100

    net: dsa: ksz: Increase the tag alignment
    Make sure to cater even for network packets with VLAN tags in them,
    increase the minimal packets size to account for those.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Cc: Woojung Huh <woojung.huh@microchip.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Tristram Ha <Tristram.Ha@microchip.com>

--- net/dsa/tag_ksz.c
+++ net/dsa/tag_ksz.c
@@ -23,7 +23,7 @@ static struct sk_buff *ksz_common_xmit(struct sk_buff *skb,
 	struct sk_buff *nskb;
 	int padlen;
 
-	padlen = (skb->len >= ETH_ZLEN) ? 0 : ETH_ZLEN - skb->len;
+	padlen = (skb->len >= VLAN_ETH_ZLEN) ? 0 : VLAN_ETH_ZLEN - skb->len;
 
 	if (skb_tailroom(skb) >= padlen + len) {
 		/* Let dsa_slave_xmit() free skb */
